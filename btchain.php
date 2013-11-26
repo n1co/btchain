6a72098e3d429926d71f20b317a131cf8df986b0
John Konolol <nico83110@gmail.com>
2013-11-26 12:54:42 +0100

    comments

--- btchain.php
+++ btchain.php
@@ -4,6 +4,7 @@ class btchain
 	function parse_json($url)
 	{
 		$data = file_get_contents($url);
+		//Next line prevent a php bug involving floating point and x64 processors (should be fixed if you use php-5.4)
 		$res = preg_replace( '/":(\d+)/', '":"\1"', $data );
 		$json = json_decode($res);
 		return $json;
